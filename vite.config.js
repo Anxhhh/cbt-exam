ea122ea2dd852ebd5bea04ffcaa656a1a6177555
Anxhhh <anshmatlotia@gmail.com>
2026-01-14 18:10:13 +0530

    Initial commit: CBT exam app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/cbt-exam/"
-});
+  base: command === "build" ? "/cbt-exam/" : "/"
+}));
